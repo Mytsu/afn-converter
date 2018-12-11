6a02c7f05984ea312d98107ab84eefc9559d0c67
saulo <saulocarvalho64@gmail.com>
2018-12-11 15:15:01 -0200

    Código Comentado

--- estrutura_afn.py
+++ estrutura_afn.py
@@ -1,8 +1,9 @@
 """
     INSTITUTO FEDERAL DE MINAS GERAIS - CAMPUS FORMIGA
     Jonathan Arantes Pinto <tlc.jooker@gmail.com>
-    Saulo Cassiano <>
+    Saulo Cassiano de Carvalho <saulocarvalho64@gmail.com>
 """
+
 
 class Estado(object):
 
@@ -34,12 +35,12 @@ class Automato(object):
             #Pegando todos iniciais e fazendo o "Inicial" tem uma ligação neles
             if self.automato[i].tipo == 0:
                 self.automato[i].tipo = 1
-                ini.transicoes.append(["None", self.automato[i].nome])
+                ini.transicoes.append(["#", self.automato[i].nome])
 
             #Pegando todos finais e ligando eles ao estado "Final"
             elif self.automato[i].tipo == 2:
                 self.automato[i].tipo = 1
-                self.automato[i].transicoes.append(["None", fin.nome])
+                self.automato[i].transicoes.append(["#", fin.nome])
 
         self.addEstado([ini.nome, ini.tipo, ini.transicoes])
         self.addEstado([fin.nome, fin.tipo, fin.transicoes])
@@ -72,7 +73,7 @@ class Automato(object):
 
         return lista_estados
 
-    #Função que verifica se o estado tem ligação saindo dele para ele mesmo, caso não tenha retornará uma string "None"
+    #Função que verifica se o estado tem ligação saindo dele para ele mesmo, caso não tenha retornará uma string "#"
     def verifica_laco_interno(self, no_deletado):
         for i in range(len(self.getAutomato())):
             if self.automato[i].nome == no_deletado:
@@ -80,7 +81,7 @@ class Automato(object):
                     if self.automato[i].transicoes[j][1] == no_deletado:
                         return self.automato[i].transicoes[j][0]    #Caso tenha ligação ele retornará o símbolo da transição
 
-        return "None"
+        return "#"
 
     #Função que verfica se existe algua transição entre dois estados, caso nao tenha retorna -1
     def verifica_se_existe_ligacao(self, estado_inicial, estado_final):
@@ -101,16 +102,16 @@ class Automato(object):
         return -1
 
     ##Função que trata os simbolos do estado de saida para o deletado e do deletado para o destino
-    #Caso os três valores sejam "None" ele retornará apenas um "None"
-    #Caso tenha pelo menos um símbolo diferente de "None" ele que será retornado
+    #Caso os três valores sejam "#" ele retornará apenas um "#"
+    #Caso tenha pelo menos um símbolo diferente de "#" ele que será retornado
     def simbolos_transicao(self, vetor_simbolos):
         simbolo = ''
         for i in range(len(vetor_simbolos)):
-            if vetor_simbolos[i] != 'None':
-                simbolo = simbolo + vetor_simbolos[i]   #Concatenando os símbolos diferentes de "None"
+            if vetor_simbolos[i] != "#":
+                simbolo = simbolo + vetor_simbolos[i]   #Concatenando os símbolos diferentes de "#"
 
-        if simbolo == '':   #Condição que verifica se a VAR símbolos tem algo ou vazia, caso esteja vazia retornará "None"
-            return "None"
+        if simbolo == '':   #Condição que verifica se a VAR símbolos tem algo ou vazia, caso esteja vazia retornará "#"
+            return "#"
         else:
             return simbolo  #Caso contrário retornará "símbolo"
 
@@ -121,7 +122,7 @@ class Automato(object):
         posicao_no = self.pega_estado(nome_no)  #Pegando a posição do estado no vetor
         self.automato[posicao_no].transicoes.pop(posicao_ligacao)   #Depois de pegar todos dados elimanará a transição de fecho de klen do estado
         for i in range(len(self.automato[posicao_no].transicoes)):
-            if self.automato[posicao_no].transicoes[i][0] != "None":    #Caso o símbolo seja diferente de apenas "None", será concatenado aos outros símbolos
+            if self.automato[posicao_no].transicoes[i][0] != "#":    #Caso o símbolo seja diferente de apenas "#", será concatenado aos outros símbolos
                 aux = simbolo + self.automato[posicao_no].transicoes[i][0]
                 self.automato[posicao_no].transicoes[i][0] = aux
             else:
@@ -131,25 +132,40 @@ class Automato(object):
 
     #Função que tratará as transições do estado a ser deletado e por fim deletará o mesmo
     def deleta_estado(self, deleta_nome):
-        if self.verifica_laco_interno(deleta_nome) != "None":   #Já tratando o caso de laço interno no estado, fecho de Klen
+        if self.verifica_laco_interno(deleta_nome) != "#":   #Já tratando o caso de laço interno no estado, fecho de Klen
             self.automato = self.remove_laco_interno(deleta_nome)
 
         posicao_no_vetor_do_no_deletado = self.pega_estado(deleta_nome) #Pega a posição do estado a ser deletado no vetor
         vertices_que_vao_ao_no = self.lista_entrada_do_vertice_deletado(deleta_nome)    #Lista com com os vértices que vão ao nó a ser deletado
         lista_transicoes_no_deletado = self.saida_do_vertice_deletado(deleta_nome)  #Lista com as transições que saem do estado a ser deletado
-
         #Nestes dois "FOR" será pego da lista do estados que vão ao no a ser deletado e as transições do no a ser deletado com os outros estados
         for i in range(len(vertices_que_vao_ao_no)):
             for j in range(len(lista_transicoes_no_deletado)):
                 destino = self.automato[posicao_no_vetor_do_no_deletado].transicoes[j][1]   #Primeiro destino de uma transição do no a ser deletado
                 if deleta_nome != destino:
+                    """
+                    #Seu Código vai vir aqui, var1 recebe a posicão da transiçaõ entre o no que tem uma transição partindo e chega ao no deletado
+                    #var2 recebe a posição da ligação do no deletado ao no destino, no caso o primeiro que achar
+                    #var1 e var2 dois sao posiçoes no vetor de transição dos nos
+                    
+                    
+                    var1 = self.verifica_se_existe_ligacao(self.automato[vertices_que_vao_ao_no[i]].nome, deleta_nome)
+                    var2 = self.verifica_se_existe_ligacao(deleta_nome, destino)
+                    print("\n")
+                    print(deleta_nome, var1, var2)
+                    print("\n")
+                    var1 = ""
+                    var2 = ""
+                    """
 
                     nome_origem = self.automato[vertices_que_vao_ao_no[i]].nome #Pegando o nome do priemiro estado no vetor que vai ao no deletado
                     aux = ''
                     if nome_origem != deleta_nome:  #Condição para evitar que pegue os símbolo de transição interno
                         aux = self.verifica_se_existe_ligacao(nome_origem, deleta_nome)
+                        var1 = self.automato[vertices_que_vao_ao_no[i]].transicoes[aux]
                         aux = self.automato[vertices_que_vao_ao_no[i]].transicoes[aux][0]   #Se nao for fecho de klen, pegará o símbolo de transição
 
+                    var2 = self.automato[posicao_no_vetor_do_no_deletado].transicoes[j]
                     simbolo_destino = self.automato[posicao_no_vetor_do_no_deletado].transicoes[j][0]   #Pegando o símbolo de transição do estado a ser deletado com o primeiro destino no vetor do "FOR"
                     aux = [aux, simbolo_destino]
                     simbolos_concatenados = self.simbolos_transicao(aux)    #tratando os simbolos pegos
@@ -157,7 +173,7 @@ class Automato(object):
                     pos_ligacao = self.verifica_se_existe_ligacao(nome_origem, destino) #pegando, se existe, a posição da ligação do no que tem transição para no deletado
                                                                                         # e do Nó a ser deletado para o No destino do Vetor
                     aux = self.automato[vertices_que_vao_ao_no[i]].transicoes[pos_ligacao][0]   #Pegando o símbolo da transição acima
-                    if pos_ligacao != int(-1) and aux != "None":    #Caso exista e o simbolo de transião seja "None" é feita uma concatenação
+                    if pos_ligacao != int(-1) and aux != "#":    #Caso exista e o simbolo de transião seja "#" é feita uma concatenação
                         aux = self.automato[vertices_que_vao_ao_no[i]].transicoes[pos_ligacao][0]
                         self.automato[vertices_que_vao_ao_no[i]].transicoes[pos_ligacao][0] = "(" + aux + \
                             "+" + simbolos_concatenados + ")"
@@ -169,95 +185,4 @@ class Automato(object):
             #"limpa_transicao" recebe a posicao da transição que está atual no "FOR" externo, caso I
             self.automato[vertices_que_vao_ao_no[i]].transicoes.pop(limpa_transicao)    #É deletado esta transição do estado origem que vai para o nó deletado
         self.automato.pop(posicao_no_vetor_do_no_deletado)  #Por fim deleta o estado deletado do vetor
-
-
-t = []
-
-automato = Automato()
-
-t.append("1")
-t.append(0)
-aux = [["a", "2"], ["b", "3"]]
-t.append(aux)
-automato.addEstado(t)
-
-t.clear()
-t.append("2")
-t.append(1)
-aux = [["a", "4"], ["b", "2"]]
-t.append(aux)
-automato.addEstado(t)
-
-t.clear()
-t.append("3")
-t.append(1)
-aux = [["b", "4"]]
-t.append(aux)
-automato.addEstado(t)
-
-t.clear()
-t.append("4")
-t.append(2)
-aux = []
-t.append(aux)
-automato.addEstado(t)
-
-automato.primeiro_passo()
-automato.modificaTransicoes()
-automato.deleta_estado('2')
-automato.deleta_estado('3')
-automato.deleta_estado('4')
-automato.deleta_estado('1')
-
-"""
-t.append("1")
-t.append(0)
-aux = [["a", "1"], ["None", "2"]]
-t.append(aux)
-automato.addEstado(t)
-
-t.clear()
-t.append("3")
-t.append(0)
-aux = [["c", "3"], ["a,d", "2"]]
-t.append(aux)
-automato.addEstado(t)
-
-t.clear()
-t.append("2")
-t.append(2)
-aux = [["b", "2"], ["None", "3"]]
-t.append(aux)
-automato.addEstado(t)
-
-automato.primeiro_passo()
-automato.modificaTransicoes()
-automato.deleta_estado("1")
-automato.deleta_estado("3")
-automato.deleta_estado("2")
-"""
-print("Final")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+
 """
     INSTITUTO FEDERAL DE MINAS GERAIS - CAMPUS FORMIGA
     Jonathan Arantes Pinto <tlc.jooker@gmail.com>
-    Saulo Cassiano <>
+    Saulo Cassiano de Carvalho <saulocarvalho64@gmail.com>
 """
 
 import json
@@ -10,7 +11,6 @@ from graphviz import Digraph
 
 """
 FORMATO DO ARQUIVO DE DADOS JSON
-
 {
     "af": {
         "estados": ["1", "2", "3"],
@@ -36,6 +36,8 @@ FORMATO DO ARQUIVO DE DADOS JSON
     retorna um tipo Automato e uma lista com a ordem dos
     estados a serem deletados
 """
+
+
 def carregar_arquivo(filename: str) -> (Automato, []):
     estados = []
     automato = Automato()
@@ -46,7 +48,7 @@ def carregar_arquivo(filename: str) -> (Automato, []):
         for e in estados:
             for inicial in data['af']['iniciais']:
                 if e.nome == inicial:
-                    e.tipo = 0            
+                    e.tipo = 0
             for final in data['af']['finais']:
                 if e.nome == final:
                     e.tipo = 2
@@ -61,6 +63,8 @@ def carregar_arquivo(filename: str) -> (Automato, []):
     Gera uma imagem do automato dado como entrada, colore
     o estado indicado e suas transicoes
 """
+
+
 def pintar_etapa(aut: Automato, etapa: int = 0, estado: str = None):
     dot = Digraph()
     dot.attr('node', shape='doublecircle')
@@ -86,6 +90,18 @@ def pintar_etapa(aut: Automato, etapa: int = 0, estado: str = None):
     dot.attr(label = 'Etapa #' + str(etapa))
     dot.render('saida/etapa' + str(etapa) + '.gv')
 
+
 if __name__ == '__main__':
     automato, lista = carregar_arquivo('teste.json')
-    pintar_etapa(automato)
+    pintar_etapa(automato, 0, automato.automato[2].nome)
+    automato.automato.sort(key=lambda x: x.tipo)    #Deixando em ordem do tipo, influencia a ordem que deleta os estados
+    automato.primeiro_passo()   #Acrescenta o estado "I" e "F"
+    automato.modificaTransicoes()   #Aqui troca "," por "+" e coloca "*"
+
+    while automato.automato[0].tipo != 0:
+        automato.deleta_estado(automato.automato[0].nome)
+
+    print("Expression Regular is: " + str(automato.automato[0].transicoes[0][0]))
+
+
+
